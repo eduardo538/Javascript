302f43eba4d01c4f8664ae5885ff6e55244af66f
eduardo538 <eduardobcr4@gmail.com>
2025-03-31 08:12:54 -0600

    Create 6.js

---
+++ 6.js
@@ -0,0 +1,8 @@
+const students = 30;
+if (students<30){
+  console.log("Pocos Alumnos");
+}else if(students<70){
+  console.log("Muy Bien, Estas en lo Normal")
+}else{
+  console.log("Demasiados Alumnos")
+}
