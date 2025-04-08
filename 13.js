c884ece30ceeb1ea94aac508605dfc8f37269beb
Eduardo Javier Baranda Cruz <eduardobcr4@gmail.com>
2025-04-07 20:38:32 -0600

    Create 13.js

---
+++ 13.js
@@ -0,0 +1,8 @@
+let password = "00000001";
+let correctPassword = "00000000";
+
+if (password=== correctPassword) {
+console.log("contraseña correcta");
+}else {
+console.log("contraseña incorrecta");
+}
