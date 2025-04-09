1142bc9aa069d87defca208edd58ad6b7d1005cd
Eduardo Javier Baranda Cruz <eduardobcr4@gmail.com>
2025-04-09 13:21:15 -0600

    Create 14.js

---
+++ 14.js
@@ -0,0 +1,8 @@
+let topping = "pepperoni";
+if (topping === "pineapple") {
+console.log("Request denied.");
+} else if (topping === "pepperoni")
+console.log("Request accepted.");
+} else {
+console.log("Can't process request.");
+}
