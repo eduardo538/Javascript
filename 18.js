528de48e5146f58cf1870d185f3055a411c5d565
Eduardo Javier Baranda Cruz <eduardobcr4@gmail.com>
2025-04-15 21:29:29 -0600

    Create 18.js

---
+++ 18.js
@@ -0,0 +1,8 @@
+const calificacion= 45;
+if (calificacion>=70 && calificación<=100){
+  console.log("Estas aprobado");
+}else if(calificación<70){
+  console.log("Na");
+}else{
+  console.log("Coloca la calificacion correcta de 1 a 100");
+}
